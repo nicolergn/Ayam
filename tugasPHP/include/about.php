111352755ee9e6865e7f224133d20c4aee4b2789
Nicole R <130779234+nicolergn@users.noreply.github.com>
2024-05-19 18:17:10 +0800

    tes tugas php

---
+++ tugasPHP/include/about.php
@@ -0,0 +1,20 @@
+<section class="aboutme" id="aboutme">
+            <div class="heading">
+                <span>A Little</span>
+                <h2>About Me</h2>
+            </div>
+            <div class="abm-container">
+                <div class="aboutme-img">
+                    <img src="asset/picture/profile/nicole_0wht.png" alt="Nicole Pakiding Photo">
+                </div>
+                <div class="aboutme-text">
+                    <h4>Hi! I'm Nicole Pakiding</h4>
+                    <p>
+                        I'm a student from Manado studying Computer Engineering at Sam Ratulangi University. I'm in my fourth semester and enjoying my time in college. In my free time, I like to go out and explore new places to see more of the world.
+                    </p>
+                    <p>
+                        I also enjoy reading stories to relax and get inspiration. I have a sweet tooth and love sweet treats. I'm enthusiastic about learning and open to new opportunities in my field.
+                    </p>
+                </div>
+            </div>
+        </section>
---
+++ tugasPHP/include/gallery.php
@@ -0,0 +1,55 @@
+<section class="gallery" id="gallery">
+            <div class="heading">
+                <span>Look at</span>
+                <h2>My Gallery</h2>
+            </div>
+            <div class="gallery-card__container">
+                <!-- ========== PHOTO 1 ========== -->
+                <div class="gallery-photo__card">
+                    <img src="asset/picture/gallery/sleeping_cat.jpg" alt="Foto Sleeping Cat" class="gallery-card__img">
+                        <div class="gallery-card__desc">
+                            <h2 class="gallery-card__title">Sleeping Cat</h2>
+                        </div>
+                    </div>
+
+                <!-- ========== PHOTO 2 ========== -->
+                <div class="gallery-photo__card">
+                    <img src="asset/picture/gallery/air_balloon.jpg" alt="Foto Air Balloon" class="gallery-card__img">
+                    <div class="gallery-card__desc">
+                        <h2 class="gallery-card__title">Air Balloon</h2>
+                    </div>
+                </div>
+
+                <!-- ========== PHOTO 3 ========== -->
+                <div class="gallery-photo__card">
+                    <img src="asset/picture/gallery/dk_heart_pose.jpg" alt="Foto DK Heart Pose" class="gallery-card__img">
+                    <div class="gallery-card__desc">
+                        <h2 class="gallery-card__title">DK Heart Pose</h2>
+                    </div>
+                </div>
+
+                <!-- ========== PHOTO 4 ========== -->
+                <div class="gallery-photo__card">
+                    <img src="asset/picture/gallery/one_dawn.jpg" alt="Foto One Dawn" class="gallery-card__img">
+                    <div class="gallery-card__desc">
+                        <h2 class="gallery-card__title">One Dawn</h2>
+                    </div>
+                </div>
+
+                <!-- ========== PHOTO 5 ========== -->
+                <div class="gallery-photo__card">
+                    <img src="asset/picture/gallery/clouds.jpg" alt="Foto Clouds" class="gallery-card__img">
+                    <div class="gallery-card__desc">
+                        <h2 class="gallery-card__title">Clouds</h2>
+                    </div>
+                </div>
+
+                <!-- ========== PHOTO 6 ========== -->
+                <div class="gallery-photo__card">
+                    <img src="asset/picture/gallery/bench.jpg" alt="Foto Bench" class="gallery-card__img">
+                    <div class="gallery-card__desc">
+                        <h2 class="gallery-card__title">A Bench</h2>
+                    </div>
+                </div>
+            </div>
+        </section>
